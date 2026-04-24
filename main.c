2895767b62f089b34f087e1e8494f9d11c0db7ca
Timur <tima.xxxyyy@mail.ru>
2026-04-24 17:34:05 +0300

    Screen to coord done + small fix
    Fixed small mistake in calculations of tangent lines
    ScreenCoord -> dekart coord and vise versa is working now

--- main.c
+++ main.c
@@ -14,14 +14,20 @@ Vector2 CalculateCollisionArea(Vector2 targetPos, Vector2 targetVec, float targe
     // float distance = sqrtf(targetX * targetX + targetY * targetY);
     float d = Vector2Length(targetPos); // d - distance to the target
     printf("Distance: %02f \n", d);
-    float m = sqrtf(d * d - targetRadius * targetRadius); // Length of the tangent line
+    float m = sqrtf(d * d - targetRadius * targetRadius); // Length of the tangent line (касательной)
     
-    float h = targetPos.x, k = targetPos.y;
-    float r = targetRadius;
+    float h = targetPos.x, k = targetPos.y; // (h, k) - pos of target
+    float r = targetRadius; // - radius of target
     Vector2 F = {m*m*h/(d*d), m*m*k/(d*d)}; // F - Точка пересечения поляры и вектора от нашего корабля к центру окружнсти
-    Vector2 s = {-(h*m*r/(d*d)), (k*m*r/(d*d))}; // s - Вектор поляры
+    Vector2 s = {-(k*m*r/(d*d)), (h*m*r/(d*d))}; // s - Вектор поляры
+                                                 //
     T1 = Vector2Add(F, s);
     T2 = Vector2Add(F, Vector2Negate(s));
+}
+
+Vector2* areaToScreen()
+{
+
 }
 
 int main(void)
@@ -30,21 +36,30 @@ int main(void)
 
     int x = 100, y = 100;
     float r = 50.0f;
-    Vector2 StartPos = {WINDOW_WIDTH/2, WINDOW_HEIGHT/2};
-    Vector2 SysTransform = {WINDOW_WIDTH/2, WINDOW_HEIGHT/2};
-    Vector2 null = {0.0f, 0.0f};
+    Vector2 shipScreenPos = { WINDOW_WIDTH/2.0f, WINDOW_HEIGHT/2.0f };
     Vector2 mousePos;
     while (!WindowShouldClose())
     {
-        mousePos = Vector2Add(GetMousePosition(), StartPos);
+        // mousePos = Vector2Add(GetMousePosition(), StartPos);
+        mousePos = GetMousePosition();
         r += GetMouseWheelMove();
-        CalculateCollisionArea(mousePos, null, r, 0.0f);
+
+        // Calculating a Decart positiong of target
+        Vector2 relativeTargetPosition = {mousePos.x - shipScreenPos.x, -(mousePos.y - shipScreenPos.y)};
+
+        CalculateCollisionArea(relativeTargetPosition, (Vector2){0, 0}, r, 0.0f);
+
+        // Calculating the positiong of resulting area on the screen
+        // areaToScreen();
+        Vector2 T1Screen = (Vector2){shipScreenPos.x + T1.x, shipScreenPos.y - T1.y};
+        Vector2 T2Screen = (Vector2){shipScreenPos.x + T2.x, shipScreenPos.y - T2.y};
+
         BeginDrawing();
             DrawText(TextFormat("targetPos: %02f %02f", mousePos.x, mousePos.y), 10, 40, 20, LIGHTGRAY);
             ClearBackground(RAYWHITE);
-            DrawCircleV(Vector2Subtract(mousePos, StartPos), r, RED); 
-            DrawLineV(StartPos, T1, BLACK);
-            DrawLineV(StartPos, T2, BLACK);
+            DrawCircleV(mousePos, r, RED); 
+            DrawLineV(shipScreenPos, T1Screen, BLACK);
+            DrawLineV(shipScreenPos, T2Screen, BLACK);
             // DrawLine(StartPos.x, 0, StartPos.x, WINDOW_HEIGHT, LIGHTGRAY);
             // DrawLine(0, StartPos.y, WINDOW_WIDTH, StartPos.y, LIGHTGRAY);
         EndDrawing();
